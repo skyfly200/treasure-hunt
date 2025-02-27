a943d862821cba551922af7f4dc82b26e5e8c663
Skyler Fly-Wilson <magic@skylerfly.com>
2025-02-26 21:45:42 -0700

    shorten menu btn

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,9 @@
 <template>
     <div>
-        <v-toolbar>
+        <v-toolbar density="compact">
             <v-toolbar-title>Treasure Hunt</v-toolbar-title>
-            <v-btn to="/leaderboard">Leaderboard</v-btn>
+            <v-btn to="/leaderboard">Scores</v-btn>
             <v-btn to="/about">About</v-btn>
-            <v-btn to="/found">Found</v-btn>
         </v-toolbar>
         <client-only>
             <Map />
