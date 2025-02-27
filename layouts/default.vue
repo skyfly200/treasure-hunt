173663afd5f6a41ff9bf5d3fead481f6b3f455e4
Skyler Fly-Wilson <magic@skylerfly.com>
2025-02-27 09:04:56 -0700

    refactor layout to include toolbar with navigation buttons

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-      <slot />
-    </div>
+  <div>
+    <v-toolbar density="compact">
+      <v-toolbar-title>Treasure Hunt</v-toolbar-title>
+      <v-btn to="/leaderboard">Scores</v-btn>
+      <v-btn to="/about">About</v-btn>
+    </v-toolbar>
+    <slot />
+  </div>
 </template>
-  
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,5 @@
 <template>
     <div>
-        <v-toolbar density="compact">
-            <v-toolbar-title>Treasure Hunt</v-toolbar-title>
-            <v-btn to="/leaderboard">Scores</v-btn>
-            <v-btn to="/about">About</v-btn>
-        </v-toolbar>
         <client-only>
             <Map />
         </client-only>
