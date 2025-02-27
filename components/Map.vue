403c8c2b620c5b11b60ce575fce74e771d6f29d9
Skyler Fly-Wilson <magic@skylerfly.com>
2025-02-26 20:23:44 -0700

    use stadia maps watercolor basemap

--- components/Map.vue
+++ components/Map.vue
@@ -10,21 +10,20 @@ onMounted(() => {
   if (!process.client || !mapContainer.value || !L) return;
 
   // Initialize the map
-  const map = L.map(mapContainer.value).setView([37.7749, -122.4194], 10); // Centered at San Francisco
+  const map = L.map(mapContainer.value).setView([37.7749, -122.4194], 5); // Centered at San Francisco
 
   // Add OpenStreetMap Tile Layer
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "&copy; OpenStreetMap contributors",
-    tileSize: 256, // Ensures proper tile size
-    maxZoom: 19,
+  L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg', {
+    maxZoom: 16,
+    attribution: '&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>', 
     timeout: 5000, // Retry loading after 5 seconds
   }).addTo(map);
 
   // Custom Marker Data
   const locations = [
+    { lat: 40.7128, lng: -74.006, title: "New York City" },
     { lat: 37.7749, lng: -122.4194, title: "San Francisco" },
     { lat: 34.0522, lng: -118.2437, title: "Los Angeles" },
-    { lat: 40.7128, lng: -74.006, title: "New York City" },
   ];
 
   // Add Markers
@@ -37,7 +36,7 @@ onMounted(() => {
     // const marker = L.marker([loc.lat, loc.lng], { icon: customIcon }).addTo(map);
 
     const marker = L.marker([loc.lat, loc.lng]).addTo(map);
-    // TODO - add custom popup content
+    // TODO - add custom popup content and open on click handler
     // marker.bindPopup(`<b>${loc.title}</b><br><img src="/custom-image.png" alt="Custom Image" />`).openPopup();
     marker.bindPopup(`<b>${loc.title}</b>`).openPopup();
   });
