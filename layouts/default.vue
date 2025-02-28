3454f93fe728ab123479418875d179c005c5acda
Skyler Fly-Wilson <magic@skylerfly.com>
2025-02-27 18:37:12 -0700

    add link to map (home) on nav bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,7 @@
   <div>
     <v-toolbar density="compact">
       <v-toolbar-title>Treasure Hunt</v-toolbar-title>
+      <v-btn to="/">Map</v-btn>
       <v-btn to="/leaderboard">Scores</v-btn>
       <v-btn to="/about">About</v-btn>
     </v-toolbar>
