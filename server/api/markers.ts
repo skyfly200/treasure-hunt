55eb42d5cafd85f979226944f9669f16514d752d
Skyler Fly-Wilson <magic@skylerfly.com>
2025-03-04 11:55:27 -0700

    move getTreasures to util

--- server/api/markers.ts
+++ server/api/markers.ts
@@ -1,3 +1,5 @@
+import { getTreasures } from '~/server/utils/treasure';
+
 /**
  * 
  * Handles the event to return a list of treasure markers.
@@ -15,17 +17,7 @@
  * - `finder`: The name of the user who found the treasure.
  */
 export default defineEventHandler(async (event) => {
-  function getTreasures() {
-    // Fetch the list of treasures from the database
-    return [
-      { id: 1, lat: 40.0150, lng: -105.2705, title: "Boulder", description: "A beautiful city at the foothills of the Rocky Mountains.", hidden: 1633035600, found: 1633039200, finder: { userId: 1, username: "User1" } },
-      { id: 2, lat: 39.7392, lng: -104.9903, title: "Denver", description: "The capital city of Colorado, known for its vibrant culture.", hidden: 1633122000, found: 1633125600, finder: { userId: 2, username: "User2" } },
-      { id: 3, lat: 39.7555, lng: -105.2211, title: "Golden", description: "A historic city with a rich mining history.", hidden: 1633208400, found: 1633212000, finder: { userId: 3, username: "User3" } },
-    ];
-  }
-
-  const treasures = getTreasures();
-
+  const treasures = await getTreasures();
   return treasures;
 });
   
--- server/utils/treasure.ts
+++ server/utils/treasure.ts
@@ -13,3 +13,12 @@ export const findTreasureByCode = (code: String) => {
     const treasure = {} as Treasure;
     return treasure;
 }
+
+export const getTreasures = () => {
+    // Get all treasure in the database
+    return [
+        { id: 1, lat: 40.0150, lng: -105.2705, title: "Boulder", description: "A beautiful city at the foothills of the Rocky Mountains.", hidden: 1633035600, found: 1633039200, finder: { userId: 1, username: "User1" } },
+        { id: 2, lat: 39.7392, lng: -104.9903, title: "Denver", description: "The capital city of Colorado, known for its vibrant culture.", hidden: 1633122000, found: 1633125600, finder: { userId: 2, username: "User2" } },
+        { id: 3, lat: 39.7555, lng: -105.2211, title: "Golden", description: "A historic city with a rich mining history.", hidden: 1633208400, found: 1633212000, finder: { userId: 3, username: "User3" } },
+    ];
+}
