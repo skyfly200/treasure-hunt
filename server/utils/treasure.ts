9f037abb1f359a9726e1fa620586eb64617020d9
Skyler Fly-Wilson <magic@skylerfly.com>
2025-03-04 16:12:32 -0700

    add actual logic to findTreasureByCode

--- server/utils/treasure.ts
+++ server/utils/treasure.ts
@@ -1,5 +1,11 @@
 import { Treasure } from '~/server/types';
 
+const treasures: Treasure[] = [
+    { code: "abcd1234", lat: 40.0150, lng: -105.2705, title: "Boulder", description: "A beautiful city at the foothills of the Rocky Mountains.", hidden: 1633035600, found: 1633039200, created_at: 1633035600, finder: { userId: 1, username: "User1" } },
+    { code: "efgh5678", lat: 39.7392, lng: -104.9903, title: "Denver", description: "The capital city of Colorado, known for its vibrant culture.", hidden: 1633122000, found: 1633125600, created_at: 1633122000, finder: { userId: 2, username: "User2" } },
+    { code: "ijkl9012", lat: 39.7555, lng: -105.2211, title: "Golden", description: "A historic city with a rich mining history.", hidden: 1633208400, found: 1633212000, created_at: 1633208400, finder: { userId: 3, username: "User3" } },
+];
+
 export const saveTreasure = (treasure: Treasure) => {
     // Save the treasure to the database
     if (false) {
@@ -11,14 +17,13 @@ export const saveTreasure = (treasure: Treasure) => {
 export const findTreasureByCode = (code: String) => {
     // Find the treasure in the database
     const treasure = {} as Treasure;
-    return treasure;
+    treasures.forEach((t) => {
+        if (t.code == code) return t;
+    });
+    return {};
 }
 
 export const getTreasures = () => {
     // Get all treasure in the database
-    return [
-        { id: 1, code: "abcd1234", lat: 40.0150, lng: -105.2705, title: "Boulder", description: "A beautiful city at the foothills of the Rocky Mountains.", hidden: 1633035600, found: 1633039200, created_at: 1633035600, finder: { userId: 1, username: "User1" } },
-        { id: 2, code: "efgh5678", lat: 39.7392, lng: -104.9903, title: "Denver", description: "The capital city of Colorado, known for its vibrant culture.", hidden: 1633122000, found: 1633125600, created_at: 1633122000, finder: { userId: 2, username: "User2" } },
-        { id: 3, code: "ijkl9012", lat: 39.7555, lng: -105.2211, title: "Golden", description: "A historic city with a rich mining history.", hidden: 1633208400, found: 1633212000, created_at: 1633208400, finder: { userId: 3, username: "User3" } },
-    ];
+    return treasures;
 }
