2c9a0db3c637118e84e220049336b7d0f7550ab7
Skyler Fly-Wilson <magic@skylerfly.com>
2025-03-05 10:51:04 -0700

    update leaderboard with new object structure

--- pages/leaderboard.vue
+++ pages/leaderboard.vue
@@ -2,6 +2,30 @@
 import { useFetch } from "#app";
 
 const { data } = await useFetch("/api/markers");
+
+// calculate found_in from hidden_at and found_at
+data = data.map((item) => {
+  if (item.found_at) {
+    item.found_in = item.found_at - item.hidden_at;
+  } else {
+    item.found_in = 0;
+  }
+  return item;
+});
+
+// sort by found_in
+data.sort((a, b) => {
+  if (a.found_in && b.found_in) {
+    return a.found_in - b.found_in;
+  } else if (a.found_in) {
+    return -1;
+  } else if (b.found_in) {
+    return 1;
+  } else {
+    return 0;
+  }
+});
+
 </script>
 
 <template>
@@ -14,22 +38,28 @@ const { data } = await useFetch("/api/markers");
           :headers="[
             { title: 'Rank', value: 'rank' },
             { title: 'Location', value: 'title' },
-            { title: 'Hidden (Time)', value: 'hidden' },
-            { title: 'Found (Time)', value: 'found' },
+            { title: 'Description', value: 'description' },
+            { title: 'Hidden (Time)', value: 'hidden_at' },
+            { title: 'Found (Time)', value: 'found_at' },
+            { title: 'Found In', value: 'found_in' },
             { title: 'Finder', value: 'finder.username' }
           ]"
-          item-value="id"
+          item-value="code"
         >
           <template v-slot:item.rank="{ index }">
             {{ index + 1 }}
           </template>
 
-          <template v-slot:item.hidden="{ item }">
-            {{ new Date(item.hidden * 1000).toLocaleString() }}
+          <template v-slot:item.hidden_at="{ item }">
+            {{ new Date(item.hidden_at * 1000).toLocaleString() }}
           </template>
 
-          <template v-slot:item.found="{ item }">
-            {{ item.found ? new Date(item.found * 1000).toLocaleString() : 'Not Found' }}
+          <template v-slot:item.found_at="{ item }">
+            {{ item.found_at ? new Date(item.found_at * 1000).toLocaleString() : 'Not Found' }}
+          </template>
+
+          <template v-slot:item.found_in="{ item }">
+            {{ item.found_in ? new Date(item.found_in * 1000).toLocaleString() : 'Not Found' }}
           </template>
 
           <template v-slot:item.finder.username="{ item }">
