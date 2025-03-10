a756289e9701c2863336911a6bd234bdab5b2b09
Skyler Fly-Wilson <magic@skylerfly.com>
2025-03-10 17:55:48 -0600

    set app.baseURL in nuxt.config.js

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  app: {
+    baseURL: '/', // Ensures correct asset paths
+  },
   modules: [
     'vuetify-nuxt-module'
   ],
